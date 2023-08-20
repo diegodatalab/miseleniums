ec5edf7a01fb61ad700516a2bd1e57dec80e9b57
Diego <diegodatalab@gmail.com>
2023-08-20 20:55:41 -0300

    running test methods using pytest

---
+++ 9_pytest/sample_package/test_sample_file.py
@@ -0,0 +1,12 @@
+
+def test_sample_one():
+    print('inside sample_one')
+
+def test_sample_two():
+    print('inside sample_two')
+
+def test_sample_three():
+    print('inside sample_three')
+
+
+# $ pytest -rA
