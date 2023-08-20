e8f1ce90bf7b8176f74496928ad91d9b158cd5b4
Diego <diegodatalab@gmail.com>
2023-08-20 13:30:02 -0300

    95. Passing Arguments To Functions

---
+++ ashutosh/8_functions/95.py
@@ -0,0 +1,19 @@
+#
+# * 95. Passing Arguments To Functions
+
+def add():
+    a = 10
+    b = 20
+    print(a+b)
+
+# ! in order to execute the code inside of a function definition, we have to call that particular function
+
+add()
+
+# ! parameters
+
+def add_param(a, b):
+    print(a+b)
+
+
+add_param(15,18)
